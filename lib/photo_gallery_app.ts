48d87a7543c766f1d03bd4a4b4ba56f532b2b989
Peter Woods <woodspeter37@gmail.com>
2025-04-12 22:11:21 +0100

    Set up processPhoto Lambda

--- lib/photo_gallery_app.ts
+++ lib/photo_gallery_app.ts
@@ -2,6 +2,14 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as events from "aws-cdk-lib/aws-lambda-event-sources";
+import * as lambdanode from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as s3n from "aws-cdk-lib/aws-s3-notifications";
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PhotoGalleryAppStack extends cdk.Stack {
@@ -20,6 +28,52 @@ export class PhotoGalleryAppStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
+    // Integration infrastructure
+    const photoProcessQueue = new sqs.Queue(this, "photoProcessQueue", {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
+    });
+
+    const photoEventsTopic = new sns.Topic(this, "PhotoEventsTopic", {
+      displayName: "New Photos topic",
+    }); 
+
+    // Lambda functions
+
+    const processPhotoFn = new lambdanode.NodejsFunction(
+      this,
+      "ProcessPhotoFn",
+      {
+        runtime: lambda.Runtime.NODEJS_22_X,
+        entry: `${__dirname}/../lambdas/processPhoto.ts`,
+        timeout: cdk.Duration.seconds(15),
+        memorySize: 128,
+      }
+    );
+
+    // S3 --> SNS
+    photosBucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3n.SnsDestination(photoEventsTopic)
+    );
+
+    // SQS --> Lambda
+    const newPhotoEventSource = new events.SqsEventSource(photoProcessQueue, {
+      batchSize: 5,
+      maxBatchingWindow: cdk.Duration.seconds(5),
+    });
+  
+    processPhotoFn.addEventSource(newPhotoEventSource);
+  
+
+    // Subs 
+
+     photoEventsTopic.addSubscription(
+      new subs.SqsSubscription(photoProcessQueue)
+    );
+
+    // Permissions
+    photosBucket.grantRead(processPhotoFn);
+
     // Outputs
 
     new cdk.CfnOutput(this, "BucketName", {
@@ -28,6 +82,10 @@ export class PhotoGalleryAppStack extends cdk.Stack {
 
     new cdk.CfnOutput(this, "TableName", {
       value: photoDataTable.tableName,
+    });
+
+    new cdk.CfnOutput(this, "PhotoEventsTopicArn", {
+      value: photoEventsTopic.topicArn,
     });
   }
 }
